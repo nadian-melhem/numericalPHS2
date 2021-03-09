f3f4b59c6dfa8e9d3e3c8cb29129a4e1c017bff3
97059 <97059@LAPTOP-0K70KH0P>
2021-03-09 11:51:46 +0200

    Add project files.

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xZero = new System.Windows.Forms.TextBox();
+            this.yZero = new System.Windows.Forms.TextBox();
+            this.run = new System.Windows.Forms.Button();
+            this.xFinal = new System.Windows.Forms.TextBox();
+            this.yFinal = new System.Windows.Forms.TextBox();
+            this.numOfIterations = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // xZero
+            // 
+            this.xZero.Location = new System.Drawing.Point(94, 26);
+            this.xZero.Name = "xZero";
+            this.xZero.Size = new System.Drawing.Size(90, 20);
+            this.xZero.TabIndex = 0;
+            // 
+            // yZero
+            // 
+            this.yZero.Location = new System.Drawing.Point(94, 74);
+            this.yZero.Name = "yZero";
+            this.yZero.Size = new System.Drawing.Size(90, 20);
+            this.yZero.TabIndex = 1;
+           // this.yZero.TextChanged += new System.EventHandler(this.yZero_TextChanged);
+            // 
+            // run
+            // 
+            this.run.Location = new System.Drawing.Point(94, 164);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(90, 23);
+            this.run.TabIndex = 2;
+            this.run.Text = "button1";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // xFinal
+            // 
+            this.xFinal.Location = new System.Drawing.Point(171, 229);
+            this.xFinal.Name = "xFinal";
+            this.xFinal.Size = new System.Drawing.Size(90, 20);
+            this.xFinal.TabIndex = 3;
+            // 
+            // yFinal
+            // 
+            this.yFinal.Location = new System.Drawing.Point(12, 229);
+            this.yFinal.Name = "yFinal";
+            this.yFinal.Size = new System.Drawing.Size(90, 20);
+            this.yFinal.TabIndex = 4;
+            // 
+            // numOfIterations
+            // 
+            this.numOfIterations.Location = new System.Drawing.Point(94, 122);
+            this.numOfIterations.Name = "numOfIterations";
+            this.numOfIterations.Size = new System.Drawing.Size(90, 20);
+            this.numOfIterations.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.numOfIterations);
+            this.Controls.Add(this.yFinal);
+            this.Controls.Add(this.xFinal);
+            this.Controls.Add(this.run);
+            this.Controls.Add(this.yZero);
+            this.Controls.Add(this.xZero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox xZero;
+        private System.Windows.Forms.TextBox yZero;
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.TextBox xFinal;
+        private System.Windows.Forms.TextBox yFinal;
+        private System.Windows.Forms.TextBox numOfIterations;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        double xPrev;
+        double yPrev;
+        double xNew, yNew;
+        public Form1()
+        {
+            InitializeComponent();
+                       
+        }
+
+        private void run_Click(object sender, EventArgs e)
+        {
+            int i = int.Parse(numOfIterations.Text);
+            xPrev = double.Parse(xZero.Text);
+            yPrev = double.Parse(yZero.Text);
+            while (i>0)
+            {
+                xNew = xPrev - ((u(xPrev, yPrev) * divVy(xPrev, yPrev) - v(xPrev, yPrev) * divUy(xPrev, yPrev)) / jacobian(xPrev, yPrev));
+                yNew = xPrev - ((v(xPrev, yPrev) * divUx(xPrev, yPrev) - u(xPrev, yPrev) * divVx(xPrev, yPrev)) / jacobian(xPrev, yPrev));
+                xPrev = xNew;
+                yPrev = yNew;
+                i--;
+
+            }
+            if (xNew.ToString() != "NaN"|| yFinal.ToString() != "NaN")
+            {
+                xFinal.Text = xNew.ToString();
+                yFinal.Text = yNew.ToString();
+            }
+            else
+                MessageBox.Show("Please Try another intervel");
+        }
+        private double u(double x,double y)
+        {
+            return Math.Pow(x-1,3)+(0.512/y);
+        }
+        private double v(double x, double y)
+        {
+            return x - 2 * Math.Sqrt(x * y);
+        }
+        private double divUx(double x,double y)
+        {
+            return 3 * Math.Pow(x - 1, 2);
+        }
+        private double divUy(double x,double y)
+        {
+            return -(0.512 / Math.Pow(y, 2));
+        }
+        private double divVx(double x,double y)
+        {
+            return 1 -( y / Math.Pow(x * y, 0.5));
+        }
+        private double divVy(double x, double y)
+        {
+            return -x / Math.Pow(x*y, 0.5);
+        }
+        private  double jacobian(double x, double y)
+        {
+            return (divUx(x, y) * divVy(x, y)) - (divUy(x, y) * divVx(x, y));
+        }
+
+        
+    }
+}
